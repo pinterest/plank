0293106a3307269eeef44d0b262b5f2a70b4c74e
rmalik <rmalik@pinterest.com>
2017-01-23 11:37:04 -0800

    Generate isEqual for Objective-C methods

--- Sources/Core/ObjCRootsRenderer.swift
+++ Sources/Core/ObjCRootsRenderer.swift
@@ -19,6 +19,8 @@ struct ObjCRootsRenderer {
         self.params = params
     }
 
+    // MARK: Properties
+
     var className: String {
         get {
             return self.rootSchema.className(with: self.params)
@@ -63,6 +65,8 @@ struct ObjCRootsRenderer {
     }
 
 
+    // MARK: Model methods
+
     func renderClassName() -> ObjCIR.Method {
         return ObjCIR.method("+ (NSString *)className") {
             ["return \(self.className.objcLiteral());"]
@@ -87,6 +91,103 @@ struct ObjCRootsRenderer {
             [
                 "return [self initWithDictionary:@{}];",
             ]
+        }
+    }
+
+    // MARK: Hash Method - Inspired from Mike Ash article on Equality / Hashing
+    // https://www.mikeash.com/pyblog/friday-qa-2010-06-18-implementing-equality-and-hashing.html
+    func renderHash() -> ObjCIR.Method {
+        func schemaHashStatement(with param: Parameter, for schema: Schema) -> String {
+            switch schema {
+            case .Enum(_), .Integer:
+                // - The value equality statement is sufficient for equality testing
+                // - All enum types are treated as Integers so we do not need to treat String Enumerations differently
+                return "_\(param)"
+            case .Float:
+                return " [@(_\(param)) hash]"
+            case .Boolean:
+                return "(_\(param) ? 1231 : 1237)"
+            case .Reference(with: let fn):
+                switch fn() {
+                case .some(.Object(let schemaRoot)):
+                    return schemaHashStatement(with: param, for: .Object(schemaRoot))
+                default:
+                    fatalError("Bad reference found for schema parameter: \(param)")
+                }
+            default:
+                return "[_\(param) hash]"
+            }
+        }
+
+        let rootHashStatement = self.isBaseClass ? ["17"] : ["[super hash]"];
+        let propReturnStatements = rootHashStatement + self.properties.map { param, schema -> String in
+            let formattedParam = param.snakeCaseToPropertyName()
+            return schemaHashStatement(with: formattedParam, for: schema)
+        }
+
+        return ObjCIR.method("- (NSUInteger)hash") {[
+            "return (",
+                -->[propReturnStatements.map{ "(\($0))" }.joined(separator: " ^\n")],
+            ");"
+        ]}
+    }
+
+    // MARK: Equality Methods inspired from NSHipster article on Equality: http://nshipster.com/equality/
+    func renderIsEqualToClass() -> ObjCIR.Method {
+        func schemaIsEqualStatement(with param: Parameter, for schema: Schema) -> String {
+            switch schema {
+            case .Integer, .Float, .Enum(_), .Boolean:
+                // - The value equality statement is sufficient for equality testing
+                // - All enum types are treated as Integers so we do not need to treat String Enumerations differently
+                return ""
+            case .Map(_):
+                return ObjCIR.msg("_\(param)", ("isEqualToDictionary", "anObject.\(param)"))
+            case .String(format: .some(.DateTime)):
+                return ObjCIR.msg("_\(param)", ("isEqualToDate", "anObject.\(param)"))
+            case .String(format: .none),
+                 .String(format: .some(.Email)),
+                 .String(format: .some(.Hostname)),
+                 .String(format: .some(.Ipv4)),
+                 .String(format: .some(.Ipv6)):
+                return ObjCIR.msg("_\(param)", ("isEqualToString", "anObject.\(param)"))
+            case .OneOf(types:_), .Object(_), .Array(_), .String(format: .some(.Uri)):
+                return ObjCIR.msg("_\(param)", ("isEqual", "anObject.\(param)"))
+            case .Reference(with: let fn):
+                switch fn() {
+                case .some(.Object(let schemaRoot)):
+                    return schemaIsEqualStatement(with: param, for: .Object(schemaRoot))
+                default:
+                    fatalError("Bad reference found for schema parameter: \(param)")
+                }
+            }
+        }
+
+        let propReturnStmts = self.properties.map { param, schema -> String in
+            let formattedParam = param.snakeCaseToPropertyName()
+            let pointerEqStmt = "_\(formattedParam) == anObject.\(formattedParam)"
+            let deepEqStmt = schemaIsEqualStatement(with: formattedParam, for: schema)
+            return [pointerEqStmt, deepEqStmt].filter { $0 != "" }.joined(separator: " || ")
+        }
+
+        return ObjCIR.method("- (BOOL)isEqualTo\(self.rootSchema.name.snakeCaseToCamelCase()):(\(self.className) *)anObject") {
+            [
+                ObjCIR.ifStmt("anObject == nil") { ["return NO;"] },
+                ObjCIR.ifStmt("[anObject isKindOfClass:[\(self.className) class]] == NO") { ["return NO;"] },
+                "return (",
+                -->[propReturnStmts.map{ "(\($0))" }.joined(separator: " &&\n")],
+                ");"
+            ]
+        }
+    }
+
+    func renderIsEqual() -> ObjCIR.Method {
+        return ObjCIR.method("- (BOOL)isEqual:(\(self.className) *)anObject") {
+            [
+                ObjCIR.ifStmt("self == anObject") { ["return YES;"] },
+                self.isBaseClass ? "" : ObjCIR.ifStmt("[super isEqual:anObject] == NO") { ["return NO;"] },
+                ObjCIR.ifStmt("[anObject isKindOfClass:[\(self.className) class]] == NO") { ["return NO;"] },
+                "return [self isEqualTo\(self.rootSchema.name.snakeCaseToCamelCase()):anObject];"
+            ].filter { $0 != "" }
         }
     }
 
@@ -121,7 +222,8 @@ struct ObjCRootsRenderer {
                 return [
                     "NSDictionary *items = \(rawObjectName);",
                     "NSMutableDictionary *\(currentResult) = [NSMutableDictionary dictionaryWithCapacity:items.count];",
-                    ObjCIR.msg("items",
+                    ObjCIR.stmt(
+                        ObjCIR.msg("items",
                                ("enumerateKeysAndObjectsUsingBlock",
                                 ObjCIR.block(["NSString *  _Nonnull key",
                                               "id  _Nonnull obj",
@@ -132,6 +234,7 @@ struct ObjCRootsRenderer {
                                         }
                                     ]
                                })
+                        )
                     ),
                     "\(propertyToAssign) = \(currentResult);"
                 ]
@@ -199,13 +302,15 @@ struct ObjCRootsRenderer {
                 "NSParameterAssert(modelDictionary);",
                 self.isBaseClass ? ObjCIR.ifStmt("!(self = [super init])") { ["return self;"] } :
                                    "if (!(self = [super initWithDictionary:modelDictionary])) { return self; }",
-                ObjCIR.msg("modelDictionary",
-                           ("enumerateKeysAndObjectsUsingBlock", ObjCIR.block(["NSString *  _Nonnull key",
+                ObjCIR.stmt(
+                    ObjCIR.msg("modelDictionary",
+                               ("enumerateKeysAndObjectsUsingBlock", ObjCIR.block(["NSString *  _Nonnull key",
                                                                                "id  _Nonnull obj",
                                                                                "__unused BOOL * _Nonnull stop"]) {
-                                        x
-                        })
-                ),
+                                                                                x
+                               }
+                    )
+                )),
                 ObjCIR.ifStmt("[self class] == [\(self.className) class]") {
                     ["[self PIModelDidInitialize:PIModelInitTypeDefault];"]
                 },
@@ -461,6 +566,8 @@ struct ObjCRootsRenderer {
     }
 
 
+    // MARK: Builder methods
+
     func renderBuilderInitWithModel() -> ObjCIR.Method {
         return ObjCIR.method("- (instancetype)initWithModel:(\(self.className) *)modelObject") {
             [
@@ -698,6 +805,9 @@ struct ObjCRootsRenderer {
                     (self.isBaseClass ? .Public : .Private, self.renderInitWithBuilderWithInitType()),
                     (.Private, self.renderDebugDescription()),
                     (.Public, self.renderCopyWithBlock()),
+                    (.Private, self.renderIsEqual()),
+                    (.Public, self.renderIsEqualToClass()),
+                    (.Private, self.renderHash()),
                     (.Public, self.renderMergeWithModel()),
                     (.Public, self.renderMergeWithModelWithInitType())
                 ],
