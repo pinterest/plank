12db0da3b00d284d936c6bcd8415dad76ea16015
Michael Schneider <maicki@users.noreply.github.com>
2017-06-16 14:19:41 -0700

    Add Flow Type Support (#57)
    * Initial support for Flow types

---
+++ Sources/Core/JSADTExtension.swift
@@ -0,0 +1,76 @@
+//
+//  JSADTExtension.swift
+//  plank
+//
+//  Created by Michael Schneider
+//
+//
+
+import Foundation
+
+extension JSModelRenderer {
+
+    public static func adtVariantTypeName(className: String, property: String) -> String {
+        return "\(className)\(property.snakeCaseToCamelCase())Type"
+    }
+
+    public static func adtCaseTypeName(_ aSchema: Schema) -> String {
+        switch aSchema {
+        case .object(let objectRoot):
+            // Intentionally drop prefix
+            return "\(objectRoot.className(with: [:]))Type"
+        case .reference(with: let ref):
+            return ref.force().map(adtCaseTypeName) ?? {
+                assert(false, "TODO: Forward optional across methods")
+                return ""
+                }()
+        case .float: return "number"
+        case .integer: return "number"
+        case .enumT(.integer): return ""  // TODO: Not supported at the moment
+        case .boolean: return "boolean"
+        case .array(itemType: _): return "Array<*>"
+        case .map(valueType: .none): return "{}"
+        case .map(valueType: .some(let valueType)) where valueType.isObjCPrimitiveType:
+            return "{ +[string]: number } /* \(valueType.debugDescription) */"
+        case .map(valueType: .some(let valueType)):
+            return "{ +[string]: \(adtCaseTypeName(valueType)) }"
+        case .string(.some(.uri)): return "PlankURI"
+        case .string(.some(.dateTime)): return "PlankDate"
+        case .string(.some), .string(.none): return "string"
+        case .enumT(.string): return "" // TODO: JS: Not supported yet
+        case .oneOf(types:_):
+            fatalError("Nested oneOf types are unsupported at this time. Please file an issue if you require this. \(aSchema)")
+        }
+    }
+
+    func renderAdtTypeRoots() -> [JSIR.Root] {
+        return self.properties.flatMap { (param, schema) -> [JSIR.Root] in
+            switch schema {
+            case .oneOf(types: let possibleTypes):
+                return [renderAdtTypeRoot(property: param, schemas: possibleTypes)]
+            case .array(itemType: .some(let itemType)):
+                switch itemType {
+                case .oneOf(types: let possibleTypes):
+                    return [renderAdtTypeRoot(property: param, schemas: possibleTypes)]
+                default: return []
+                }
+            case .map(valueType: .some(let additionalProperties)):
+                switch additionalProperties {
+                case .oneOf(types: let possibleTypes):
+                    return [renderAdtTypeRoot(property: param, schemas: possibleTypes)]
+                default: return []
+                }
+            default: return []
+            }
+        }
+    }
+
+    func renderAdtTypeRoot(property: String, schemas: [Schema]) -> JSIR.Root {
+        let types = schemas.map { (schema) -> String in
+            return JSModelRenderer.adtCaseTypeName(schema)
+        }.joined(separator: " | ")
+
+        // Just return it as a string for now
+        return JSIR.Root.macro("export type \(JSModelRenderer.adtVariantTypeName(className: className, property: property)) = \(types);")
+    }
+}
--- Utility/integration-test.sh
+++ Utility/integration-test.sh
@@ -6,6 +6,15 @@ JSON_FILES=`ls -d Examples/PDK/*.json`
 # Generate Objective-C models
 .build/debug/plank  --output_dir=Examples/Cocoa/Sources/objc/ $JSON_FILES
 
+# Generate flow types for models
+.build/debug/plank --lang flow  --output_dir=Examples/JS/flow/ $JSON_FILES
+# Verify flow types
+if [ -x "$(command -v flow)" ]; then
+  pushd Examples/JS/flow
+  flow
+  popd
+fi
+
 # Move headers in the right place for the Swift PM
 mv Examples/Cocoa/Sources/objc/*.h Examples/Cocoa/Sources/objc/include
 
