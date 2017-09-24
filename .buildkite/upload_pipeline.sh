ce6d1af1dad3e48322afd47b3a294c8f81988dca
Rahul Malik <rmalik@pinterest.com>
2017-09-24 10:39:12 -0700

    Fix script bug

--- .buildkite/upload_pipeline.sh
+++ .buildkite/upload_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Only run pipeline uploads in buildkite environments
-if [[ -e ${BUILDKITE_BUILD_NUMBER} ]];
+if [[ -e ${BUILDKITE_BUILD_NUMBER} ]]; then
     buildkite-agent pipeline upload .buildkite/plank-pipeline.yml
 fi
 
