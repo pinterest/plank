7328fa0cb6c2bf14e219476804cf892ddd82f4ef
Michael Schneider <maicki@users.noreply.github.com>
2018-08-16 00:35:15 +0200

    Refactor version number to use a struct (#145)

--- Sources/plank/Version.swift
+++ Sources/plank/Version.swift
@@ -1,1 +1,14 @@
-let PLANK_VERSION = "1.4.1"
+//
+//  Version.swift
+//  Plank
+//
+//  Created by Michael Schneider on 8/15/18.
+//
+
+import Foundation
+
+public struct Version {
+    public let value: String
+    
+    public static let current = Version(value: "1.4.1")
+}
