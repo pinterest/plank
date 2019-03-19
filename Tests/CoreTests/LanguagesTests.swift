71fd8c16eae5aac26a690779674f846c0732726b
Rico Yao <ryao@pinterest.com>
2019-03-19 15:59:11 -0700

    make format

--- Tests/CoreTests/LanguagesTests.swift
+++ Tests/CoreTests/LanguagesTests.swift
@@ -10,13 +10,12 @@ import XCTest
 @testable import Core
 
 class LanguagesTests: XCTestCase {
-    
     func testSnakeCaseToCamelCase() {
         XCTAssert(Languages.objectiveC.snakeCaseToCamelCase("created_at") == "CreatedAt")
         XCTAssert(Languages.objectiveC.snakeCaseToCamelCase("created_aT") == "CreatedAT")
         XCTAssert(Languages.objectiveC.snakeCaseToCamelCase("Created_At") == "CreatedAt")
     }
-    
+
     func testSnakeCaseToPropertyName() {
         XCTAssert(Languages.objectiveC.snakeCaseToPropertyName("created_at") == "createdAt")
         XCTAssert(Languages.objectiveC.snakeCaseToPropertyName("CreatedAt") == "createdAt")
@@ -27,16 +26,16 @@ class LanguagesTests: XCTestCase {
         XCTAssert(Languages.objectiveC.snakeCaseToPropertyName("Test_url") == "testURL")
         XCTAssert(Languages.objectiveC.snakeCaseToPropertyName("url_test") == "urlTest")
     }
-    
+
     func testSnakeCaseToCapitalizedPropertyName() {
         XCTAssert(Languages.objectiveC.snakeCaseToCapitalizedPropertyName("created_at") == "CreatedAt")
         XCTAssert(Languages.objectiveC.snakeCaseToCapitalizedPropertyName("CreatedAt") == "CreatedAt")
     }
-    
+
     func testReservedKeywordSubstitution() {
         XCTAssert(Languages.objectiveC.snakeCaseToCapitalizedPropertyName("nil") == "NilProperty")
         XCTAssert(Languages.java.snakeCaseToCapitalizedPropertyName("nil") == "Nil")
-        
+
         XCTAssert(Languages.objectiveC.snakeCaseToCapitalizedPropertyName("null") == "Null")
         XCTAssert(Languages.java.snakeCaseToCapitalizedPropertyName("null") == "NullProperty")
     }
